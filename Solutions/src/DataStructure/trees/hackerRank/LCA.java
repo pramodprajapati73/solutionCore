a90580231bea371774dd83215be6b8231077d790
pramodprajapati73 <pramodprajapati73@yahoo.in>
2018-10-15 22:33:50 +0530

    hackerRank problem - refactoring

--- Solutions/src/DataStructure/trees/hackerRank/LCA.java
+++ Solutions/src/DataStructure/trees/hackerRank/LCA.java
@@ -15,7 +15,7 @@ class Node {
 public class LCA {
     public static Node lca(Node root, int v1, int v2) {
         if(root==null)
-            return root;
+            return null;
         else if(v1<root.data & v2<root.data)
             return lca(root.left, v1,v2);
         else if(v1>root.data & v2>root.data)
